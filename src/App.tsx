1aa9b85067e1ef616718f329555c6bd9351de433
Abhinav Yadav <actcrick02@gmail.com>
2022-07-09 15:28:48 +0530

    built the canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Container, Heading } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import CanvasBoard from "./components/CanvasBoard";
+// import ScoreCard from "./components/ScoreCard";
+// import store from "./store";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Provider store={store}>
+      <ChakraProvider>
+        <Container maxW="container.lg" centerContent>
+          <Heading as="h1" size="xl">SNAKE GAME</Heading>
+          <CanvasBoard height={600} width={1000} /> //Canvasboard component added 
+        </Container>
+      </ChakraProvider>
+    // </Provider>
   );
-}
+};
 
 export default App;
